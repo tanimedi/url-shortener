39602ade429cc0bbcb06c55a206643bb6c1e7af9
Tanisha <tjmedi87@gmail.com>
2021-06-05 22:25:31 -0400

    added schema, created short url, save document

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const { nanoid } = require('nanoid');
+var dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const uri = process.env.DB_URI
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 
 app.use(cors());
 
@@ -18,6 +30,39 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+//URL Shortener
+let urlSchema = new mongoose.Schema({
+  originalURL = String,
+  shortURL = String
+})
+let urls = mongoose.model('urls', urlSchema);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/shorturl', (req, res) => {
+let enteredURL= req.body.url;
+dns.lookup(enteredURL, err => {
+  if(err) {
+    res.json({ error: 'invalid url' })
+  }else{
+    let urlEnding = nanoid(10);
+    let shortener = new urls({
+      original_url: enteredURL,
+      short_url: __dirname + "/api/shorturl/" + urlEnding
+    })
+    shortener.save((err, doc) => {
+      if (err) console.log('error');
+      res.json({
+        original_url: enteredURL,
+        short_url: shortener.short_url
+      })
+    })
+  }
+})
+})
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
