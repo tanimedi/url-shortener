71350dfdfe28b357c3ed1a40f58e85b89ec50017
Tanisha <tjmedi87@gmail.com>
2023-04-30 23:56:48 -0400

    add ejs

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var url = require("url");
 
 
 // Basic Configuration
-// app.set('views', path.join(__dirname,'views'));
-
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
 const port = process.env.PORT || 3000;
 
 mongoose
